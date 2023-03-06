132b2cfaba43162a383505046875cd241684dfcd
Eshban Suleman <eshban9492@gmail.com>
2023-03-06 22:41:43 +0500

    Hyperparameter Optimization w/ Optuna

--- best_model_config.py
+++ best_model_config.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 from preprocess import clean_text
@@ -9,13 +8,13 @@ from sklearn.model_selection import train_test_split, cross_val_score
 
 from sklearn.feature_extraction.text import CountVectorizer
 
-from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 
 def objective(trial):
     df = pd.read_csv("spam_ham_dataset.csv")
-    
+    df["text"] = df["text"].apply(lambda x: clean_text(x))
     X = df["text"]
     y = df["label_num"]
 
@@ -28,36 +27,28 @@ def objective(trial):
     classifier_name = trial.suggest_categorical(
         "classifier",
         [
-            "LogisticRegression",
             "RandomForest",
-            "SVC"
+            "SVC",
+            "NaiveBayes"
         ]
     )
 
-    if classifier_name == "LogisticRegression":
-        lr_penalty = trial.suggest_categorical(
-            "penalty", 
-            [
-                "l1",
-                "l2",
-                "elasticnet"
-            ],
-            log=True
-        )
-        classifier_obj = LogisticRegression(penalty=lr_penalty, random_state=42)
-    elif classifier_name == "RandomForest":
+    if classifier_name == "RandomForest":
         rf_max_depth = trial.suggest_int("rf_max_depth", 2, 32, log=True)
         rf_n_estimators = trial.suggest_int("rf_n_estimators", 2, 32, log=True)
         classifier_obj = RandomForestClassifier(max_depth=rf_max_depth, n_estimators=rf_n_estimators)
     elif classifier_name == "SVC":
         svc_c = trial.suggest_float("svc_c", 1e-10, 1e10, log=True)
         classifier_obj = SVC(C=svc_c, gamma="auto")
+    else:
+        classifier_obj = MultinomialNB()
     
     score = cross_val_score(classifier_obj, X_train, y_train, n_jobs=-1, cv=3)
     acc = score.mean()
     return acc
 
 study = optuna.create_study(direction="maximize")
-study.optimize(objective, n_trials=100)
+study.optimize(objective, n_trials=50)
 
-print(study.best_trial)
+print(f"\nBest Trial:\n{study.best_trial}")
+print(f"\nBest Params:\n{study.best_params}")
