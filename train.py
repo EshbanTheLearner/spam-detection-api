21e44681a33865b2de8ca49f81cd21969db361d3
Eshban Suleman <eshban9492@gmail.com>
2023-03-07 21:30:21 +0500

    train test split

--- train.py
+++ train.py
@@ -12,3 +12,11 @@ def load_data(path):
     X = df["text"]
     y = df["label_num"]
     return X, y
+
+filepath = "spam_ham_dataset.csv"
+X, y = load_data(filepath)
+vectorizer = CountVectorizer()
+vectorizer.fit(X)
+X = vectorizer.transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
