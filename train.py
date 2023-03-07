4d6996269da597b9b08a55f3b7b57f34460148ce
Eshban Suleman <eshban9492@gmail.com>
2023-03-07 20:13:53 +0500

    Defined data loading function

--- train.py
+++ train.py
@@ -5,3 +5,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import wandb
 import pickle
+
+def load_data(path):
+    df = pd.read_csv(path)
+    df["text"] = df["text"].apply(lambda x: clean_text(x))
+    X = df["text"]
+    y = df["label_num"]
+    return X, y
