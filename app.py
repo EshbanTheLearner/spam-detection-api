204b783c0def99ee4598bef3e47ab6b771bc2d86
Eshban Suleman <eshban9492@gmail.com>
2023-03-11 17:14:31 +0500

    API using FastAPI

--- app.py
+++ app.py
@@ -1,3 +1,35 @@
 from fastapi import FastAPI, HTTPException
 from joblib import load
+from preprocess import clean_text
 
+vectorizer = load(open("./models/count_vectorizer.joblib", "rb"))
+clf = load(open("./models/naivebayes_clf.joblib", "rb"))
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {
+        "message": "Welcome to Spam Detection API"
+    }
+
+@app.post("/predict")
+def predict(text):
+    prediction = ""
+    if not(text):
+        raise HTTPException(
+            status_code=400,
+            detail="Please Provide a Valid Text Message"
+        )
+    text = clean_text(text)
+    pred = clf.predict(vectorizer.transform([text]))
+
+    if pred[0] == 0:
+        prediction = "Ham"
+    elif pred[0] == 1:
+        prediction = "Spam"
+
+    return {
+        "text": text,
+        "prediction": prediction
+    }
